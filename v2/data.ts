7895db333de9bdc7ac7becb4ffb386d43b1fd7d9
JaeHo Song <thruthesky@gmail.com>
2016-12-06 18:08:00 +0900

    commenting on data.ts 2

--- v2/data.ts
+++ v2/data.ts
@@ -108,7 +108,6 @@ export class Data extends Api {
                 }, error => {
                     alert( error );
                 } );
-
      * @endcode
      */
     delete( data: { idx?: any, gid?: string }, successCallback: (re:FILE_DELETE_RESPONSE) => void, failureCallback: ( error: string) => void ) {
