1cd5cbaac5f8ae0263e79aeb298b574964c6032c
JaeHo Song <thruthesky@gmail.com>
2018-05-25 13:20:22 +0800

    update for post list

--- components/forum/post-list/post-list.component.ts
+++ components/forum/post-list/post-list.component.ts
@@ -2,6 +2,10 @@ import { Component, AfterViewInit } from '@angular/core';
 import { PhilGoApiService } from '../../../philgo-api.module';
 import { ApiPostListOption, ApiPostData } from '../../../providers/philgo-api.service';
 
+interface Result {
+    idxes: Array<string>;
+    posts: { [idx: string]: ApiPostData };
+}
 @Component({
     selector: 'app-post-list-component',
     templateUrl: 'post-list.component.html'
@@ -18,37 +22,48 @@ export class PostListComponent implements AfterViewInit {
         post_id: '' // post id
     };
 
-    re: {
-        idxes: Array<string>;
-        posts: { [idx: string]: ApiPostData };
-    } = {
-            idxes: [],
-            posts: {}
-        };
+    re: Result = null;
     constructor(
         public api: PhilGoApiService
     ) {
-
+        this.init();
     }
     ngAfterViewInit() {
         // console.log('postId: ', this.postId);
     }
+    init() {
+        this.re = {
+            idxes: [],
+            posts: {}
+        };
+    }
+    /**
+     * @desc Re-initialization
+     *      1. option.page_no
+     *          If option.page_no is set, then it will initialize the previous page.
+     *          So, when you want to list posts from a new post_id, you can set option.page_no to 1.
+     *      2. option.post_id
+     *          If option.post_id is set and it is different from this.option.post_id, then it re-initialize.
+     * @param option post list optoin
+     */
     loadPage(option: ApiPostListOption) {
         if (option.limit) {
             this.option.limit = option.limit;
         }
         if (option.page_no) {
             this.option.page_no = option.page_no;
+            this.init();
         }
-        if (option.post_id) {
+        if (option.post_id && option.post_id !== this.option.post_id ) {
             this.option.post_id = option.post_id;
+            this.init();
         }
 
         this.api.postList(this.option).subscribe(res => {
             console.log('postList(): res: ', res);
             if (res.posts && res.posts.length) {
                 for (const post of res.posts) {
-                    this.re.idxes.push( post.idx );
+                    this.re.idxes.push(post.idx);
                     this.re.posts[post.idx] = post;
                 }
             }
